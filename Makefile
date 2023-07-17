29b96bc9f70bf533fc68d059865f1d86568f361f
Igor-Makeev <Igormakeev2017@gmail.com>
2023-07-17 16:53:09 +1000

    added idx to clickhouse migrations schema

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ migrate:
 	migrate -path internal/migrations/postgres -database "postgres://postgres:asdasd@localhost:5436/postgres?sslmode=disable" -verbose $(type)
 
 clickhouse_migrate:
-	migrate -path internal/migrations/clickhouse -database "clickhouse://localhost:9000?debug=true" -verbose $(type)
+	migrate -path internal/migrations/clickhouse -database "clickhouse://localhost:9000?debug=true&x-multi-statement=true" -verbose $(type)
 
--- internal/migrations/clickhouse/000001_table.up.sql
+++ internal/migrations/clickhouse/000001_table.up.sql
@@ -1,15 +1,16 @@
 
-CREATE TABLE IF NOT EXISTS events
-(
- id         int,
+CREATE TABLE IF NOT EXISTS events(
+id         int,
 campaign_id  int,
 name String,
 description String,
 priority int,
 removed Bool ,
-created_at TIMESTAMP 
-)ENGINE = Log;
-
-
+EventTime TIMESTAMP,
+INDEX index_id_events id TYPE minmax GRANULARITY 3,
+INDEX index_campaign_id_events campaign_id TYPE minmax GRANULARITY 3,
+INDEX index_name_events name TYPE bloom_filter GRANULARITY 3
+)ENGINE = MergeTree
+ORDER BY EventTime;
 
 
